5ba1af913a369fc3c11087d11952af054451351b
yanghe <yhe1107@hotmail.com>
2021-11-17 22:57:39 -0500

    Updated

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tinder News: Android Mobile App
+• Designed the Instagram style News app based on Google component Architectural MVVM Pattern
+
+• Implemented the bottom bar & page navigation using the JetPack navigation component
+
+• Utilized Mindork’s PalceHolderView to support swipe gestures for liking/disliking the news
+
+• Built the room database with LiveData & ViewModel to support a local cache and offline model
+
+• Imtegrated Retrofit and Rxjava to pull the latest news data from a RESTful endpoint
---
+++ app/src/main/java/com/tinnews/ui/search/SearchViewModel.java
@@ -0,0 +1,42 @@
+package com.tinnews.ui.search;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+import com.tinnews.model.Article;
+import com.tinnews.model.NewsResponse;
+import com.tinnews.repository.NewsRepository;
+
+public class SearchViewModel extends ViewModel {
+    private final NewsRepository repository;
+    private final MutableLiveData<String> searchInput = new MutableLiveData<>();
+    private final MutableLiveData<Article> favoriteArticleInput = new MutableLiveData<>();
+
+
+    public SearchViewModel(NewsRepository repository) {
+        this.repository = repository;
+    }
+
+    public void setSearchInput(String query) {
+        searchInput.setValue(query);
+    }
+
+    public void setFavoriteArticleInput(Article article) {
+        favoriteArticleInput.setValue(article);
+    }
+
+    public LiveData<NewsResponse> searchNews() {
+        return Transformations.switchMap(searchInput, repository::searchNews);
+    }
+
+    public LiveData<Boolean> onFavorite() {
+        return Transformations.switchMap(favoriteArticleInput, repository::favoriteArticle);
+    }
+
+    public void onCancel() {
+        repository.onCancel();
+    }
+
+}
